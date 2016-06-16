6df7ef8eb3a9a1c3fc4e28387979a079599a36df
Fan Yu <yfann2012@163.com>
2016-06-16 22:56:43 +0800

    update

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -9,71 +9,25 @@ import {connect} from 'react-redux'
 
 
 var ManageAuthorPage=React.createClass({
-    mixins:[
-        Router.Navigation
-    ],
-    statics:{
-        willTransitionFrom:function (transition,component) {
-
-        }
-    },
-    getInitialState:function () {
-        return{
-            author:{id:'',firstName:'',lastName:''},
-            errors:{},
-            dirty:false
-        };
-    },
-    componentWillMount:function () {
-        var authorId=this.props.params.id;
-        if(authorId){
-            this.setState({author:AuthorStore.getAuthorById(authorId)});
-        }
-    },
-    setAuthorState:function (event) {//called when key press
-        this.setState({dirty:true});
-        var field=event.target.name;
-        var value=event.target.value;
-        this.state.author[field]=value;
-        return this.setState({author:this.state.author});
-    },
-    authorFormIsValid:function () {
-        var formIsValid=true;
-        this.state.errors={};
-        if(this.state.author.firstName.length<3){
-            this.state.errors.firstName='First name must be at least 3 characters.';
-            formIsValid=false;
-        }
-        if(this.state.author.lastName.length<3){
-            this.state.errors.lastName='Last name must be at least 3 characters.';
-            formIsValid=false;
-        }
-        
-        this.setState({errors:this.state.errors});
-        return formIsValid;
-    },
     saveAuthor:function (event) {
         event.preventDefault();
-        
-        if(!this.authorFormIsValid()){
-            return;
-        }
+
         if(this.props.author.id){
             this.props.updateAuthor(this.state.author);
         }else{
             this.props.createAuthor(this.state.author);
         }
-
-        this.setState({dirty:false});
         toastr.success('Author saved.');
-        this.transitionTo('authors');
+    },
+    setAuthor:function(){
+        
     },
     render:function () {
         return (
             <AuthorForm author={this.props.author}
-                        onChange={this.setAuthorState}
+                        onChange={this.setAuthor}
                         onSave={this.saveAuthor}
-                        errors={this.state.errors}/>
+                        errors={this.props.errors}/>
         );
     },
     propTypes:{
@@ -86,7 +40,8 @@ var ManageAuthorPage=React.createClass({
 
 const mapStateToProps=(state)=>{
     return {
-        authors:state
+        author:{},
+        errors:{}
     }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 
-$ = jQuery = require('jquery');
 var React = require('react');
 var Router=require('react-router');
 var routes=require('./routes');
-var InitializeActions=require('./actions/initializeActions');
+import {Provider} from 'react-redux';
+import {createStore,applyMiddleware} from 'redux'
+import reducer from './reducers'
 
-InitializeActions.initApp();
-
-//Router.HistoryLocation is optional
 Router.run(routes,Router.HistoryLocation,function(Handler){
-    React.render(<Handler/>,document.getElementById('app'));
+    React.render(
+       <Provider store={store}>
+        <Handler/>
+       </Provider>,
+      document.getElementById('app'));
 });
 
 
